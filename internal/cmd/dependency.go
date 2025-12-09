cb2b55dc166b3d3f690ec6943ac371e6f71c607c
Rangel Reale <rangelspam@gmail.com>
2025-12-09 12:19:24 -0300

    diff

--- internal/cmd/dependency.go
+++ internal/cmd/dependency.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
+	"strings"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/rrgmc/helm-vendor/internal/helm"
 	"helm.sh/helm/v3/pkg/chartutil"
 	"sigs.k8s.io/yaml"
@@ -75,13 +76,58 @@ func DependencyDiff(ctx context.Context, path string) error {
 		IsUpgrade: false,
 	}
 
-	valuesToRender, err := chartutil.ToRenderValues(chart, emptyValues, releaseOptions, nil)
+	// valuesToRender, err := chartutil.ToRenderValues(chart, emptyValues, releaseOptions, nil)
+	_, err = chartutil.ToRenderValues(chart, emptyValues, releaseOptions, nil)
 	if err != nil {
 		return err
 	}
 
-	spew.Dump(values)
-	spew.Dump(valuesToRender)
+	mapIterate(values, func(path []string, value any) {
+		fmt.Printf("%s : %v\n", strings.Join(path, "."), value)
+	})
+
+	// spew.Dump(values)
+	// spew.Dump(valuesToRender)
 
 	return nil
+}
+
+func mapIterate(m map[string]any, f func(path []string, value any)) {
+	mapIteratePath(m, nil, f)
+}
+
+func mapIteratePath(m map[string]any, startPath []string, f func(path []string, value any)) {
+	for k, v := range m {
+		currentPath := slices.Concat(startPath, []string{k})
+
+		// If the value is another map, recurse
+		if nextMap, isMap := v.(map[string]any); isMap {
+			mapIteratePath(nextMap, currentPath, f)
+		} else {
+			f(currentPath, v)
+		}
+	}
+}
+
+func findRecursive(data map[string]any, path []string) (interface{}, bool) {
+	if len(path) == 0 {
+		return data, true // Reached the target level
+	}
+
+	currentKey := path[0]
+	val, ok := data[currentKey]
+	if !ok {
+		return nil, false // Key not found at this level
+	}
+
+	if len(path) == 1 {
+		return val, true // Found the final value
+	}
+
+	// If the value is another map, recurse
+	if nextMap, isMap := val.(map[string]any); isMap {
+		return findRecursive(nextMap, path[1:])
+	}
+
+	return nil, false // Key found, but not a map for further recursion
 }

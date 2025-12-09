ea7457a04d4873f4cb2d17779bf276924948e53b
Rangel Reale <rangelspam@gmail.com>
2025-12-09 11:49:55 -0300

    diff

--- internal/cmd/dependency.go
+++ internal/cmd/dependency.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/rrgmc/helm-vendor/internal/helm"
+	"helm.sh/helm/v3/pkg/chartutil"
+	"sigs.k8s.io/yaml"
 )
 
 func Dependency(ctx context.Context, path string, allVersions bool) error {
@@ -32,8 +34,22 @@ func Dependency(ctx context.Context, path string, allVersions bool) error {
 }
 
 func DependencyDiff(ctx context.Context, path string) error {
+	values := chartutil.Values{}
+	var valuesErr error
+
 	chart, err := helm.LoadDir(path, func(name string, fi os.FileInfo) bool {
 		if name == "values.yaml" {
+			valueFile := filepath.Join(path, name)
+			bytes, err := os.ReadFile(valueFile)
+			if err != nil {
+				valuesErr = err
+				return false
+			}
+			if err := yaml.Unmarshal(bytes, &values); err != nil {
+				valuesErr = fmt.Errorf("failed to parse %s: %w", valueFile, err)
+				return false
+			}
+
 			return false
 		}
 		return true
@@ -41,8 +57,31 @@ func DependencyDiff(ctx context.Context, path string) error {
 	if err != nil {
 		return err
 	}
+	if valuesErr != nil {
+		return valuesErr
+	}
 
-	spew.Dump(chart)
+	emptyValues := chartutil.Values{}
+
+	if err := chartutil.ProcessDependencies(chart, emptyValues); err != nil {
+		return err
+	}
+
+	releaseOptions := chartutil.ReleaseOptions{
+		Name:      chart.Metadata.Name,
+		Namespace: "default",
+		Revision:  1,
+		IsInstall: true,
+		IsUpgrade: false,
+	}
+
+	valuesToRender, err := chartutil.ToRenderValues(chart, emptyValues, releaseOptions, nil)
+	if err != nil {
+		return err
+	}
+
+	spew.Dump(values)
+	spew.Dump(valuesToRender)
 
 	return nil
 }
